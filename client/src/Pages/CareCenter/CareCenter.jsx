c4a4131a945cf2a3ba8af28a599adef594937bce
Francisco Santiago <fcosantiagoc@gmail.com>
2023-03-05 21:57:33 -0600

    resolve conflicts in index.jsx and config of care center

--- client/src/Pages/CareCenter/CareCenter.jsx
+++ client/src/Pages/CareCenter/CareCenter.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import Swal from 'sweetalert2';
-import Select from 'react-select'
-=======
 import React, {useState, useEffect} from 'react';
 import { useSearchParams , useLocation } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import Select from 'react-select';
->>>>>>> develop
 import { columns, data, dataAgency, downloadCSV } from './centersconfig';
 import DataTable from 'react-data-table-component';
 
